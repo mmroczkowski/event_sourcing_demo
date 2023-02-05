21ef8ddc43cab01a0cd2b89bbd5ae9e324ef3437
mmroczkowski <marcinmroczkowski@o2.pl>
2023-02-05 21:38:43 +0100

    Added Spring Boot and dependencies.

---
+++ src/main/java/org/demo/EventSourcingDemo.java
@@ -0,0 +1,11 @@
+package org.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(EventSourcingDemo.class, args);
+    }
+}
